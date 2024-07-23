d90676d5413a20014ad5fabfbb0f93be93cc2ad7
Nathan Edmund Fanlau <nathanfanlau@gmail.com>
2024-07-23 13:02:49 +0700

    Add feature to log table to save the fall images

--- app_phonecam.py
+++ app_phonecam.py
@@ -3,6 +3,7 @@ import streamlit as st
 from ultralytics import YOLO
 from datetime import datetime, timedelta
 import pandas as pd
+import os
 
 def app():
     st.header('SafeFall: Fall Detection Web App')
@@ -66,13 +67,20 @@ def app():
                     if object_name == 'Fall Detected':
                         current_time = datetime.now()
                         if current_time - st.session_state.last_fall_time > timedelta(seconds=10):
-                            fall_date = current_time.strftime('%d/%m/%Y')
-                            fall_time = current_time.strftime('%H:%M:%S')
+                            fall_date = current_time.strftime('%Y-%m-%d')
+                            fall_time = current_time.strftime('%H-%M-%S')
+                            
+                            frame_filename = f'fall_frame_{fall_date}_{fall_time}_{score:.2f}.png'
+                            frame_path = os.path.join('fall_frames', frame_filename)
+                            os.makedirs('fall_frames', exist_ok=True)
+                            cv2.imwrite(frame_path, frame)
+                            
                             st.session_state.fall_history.append({
                                 'No': len(st.session_state.fall_history) + 1,
                                 'Date': fall_date,
-                                'Time (WIB)': fall_time,
-                                'Confidence Score': f'{score:.2f}'
+                                'Time (WIB)': fall_time.replace(':', '-'),
+                                'Confidence Score': f'{score:.2f}',
+                                'Image': frame_path
                             })
                             st.session_state.last_fall_time = current_time
 
@@ -89,7 +97,17 @@ def app():
     if st.session_state.fall_history:
         df_fall_history = pd.DataFrame(st.session_state.fall_history)
         df_fall_history.set_index('No', inplace=True)
-        st.table(df_fall_history)
+        # st.table(df_fall_history)
+        
+        # Display the table with images
+        for index, row in df_fall_history.iterrows():
+            st.write(f"**No:** {index}")
+            st.write(f"**Date:** {row['Date']}")
+            st.write(f"**Time (WIB):** {row['Time (WIB)']}")
+            st.write(f"**Confidence Score:** {row['Confidence Score']}")
+            st.image(row['Image'])
+            st.write("---")
+        
     else:
         st.write('No falls detected yet.')
 
